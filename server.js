66b9a1a57977fdf005cf497a36f62e5d982a869d
Eric Penrod <emp923@gmail.com>
2025-05-02 14:02:17 -0400

    mongoDB connection

--- server.js
+++ server.js
@@ -6,11 +6,16 @@ const errorHandler = require('./middleware/errorHandler');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const corsOptions = require('./config/corsOptions');
+const connectDB = require('./config/dbConn');
+const mongoose = require('mongoose');
+const { logEvents } = require('./middleware/logger');
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 
 console.log(process.env.NODE_ENV);
+
+connectDB();
 
 app.use(logger);
 
@@ -37,6 +42,14 @@ app.all('*', (req, res) => {
 
 app.use(errorHandler);
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+mongoose.connection.once('open', () => {
+  console.log('Connected to MongoDB');
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+});
+
+mongoose.connection.on('error', err => {
+  console.log(err);
+  logEvents(`${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`, 'mongoErrLog.log');
 });
