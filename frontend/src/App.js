6c4aca5a26fd5c8086a6f7332b52bdd7cedbffa8
Eric Penrod <emp923@gmail.com>
2025-05-05 17:46:18 -0400

    public page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,12 @@
 import { Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
+import Public from './components/Public';
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
-        
+        <Route index element={<Public />} />
       </Route>
     </Routes>
   );
