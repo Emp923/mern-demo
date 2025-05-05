d6b00ba58cd1ffbcade177dd8377e2f8460c1bc3
Eric Penrod <emp923@gmail.com>
2025-05-05 18:03:49 -0400

    dash layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,20 @@
 import { Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import Public from './components/Public';
+import Login from './features/auth/Login';
+import DashLayout from './components/DashLayout';
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Public />} />
+        <Route path="login" element={<Login />} />
+
+        <Route path="dash" element={<DashLayout />}>
+          {/* protected routes here */}
+        </Route>
+
       </Route>
     </Routes>
   );
